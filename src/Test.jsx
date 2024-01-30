8f6ce9f1dfb1b9f004d30c620c71f62e48f017a7
LokeshMohanMahankali <Lokeshmohan@email.com>
2024-01-30 18:48:53 +0530

    intial-commite

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,13 +1,22 @@
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 
 const Test = () => {
+  const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
+
   return (
-    <div className='course'>
-    <motion.div class="box" 
-      initial={{opacity: 0, scale: 0.5}}
-      animate={{opacity: 0, scale: 1}} 
-      transition={{duration: 2, delay: 1}}
-    ></motion.div>
+    <div className="course">
+      <motion.div
+        class="box"
+        initial={{ opacity: 0.5, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 2, delay: 1 }}
+        // whileHover={{ opacity: 1, scale: 2 }}
+        whileTap={{}}
+        drag
+      ></motion.div>
     </div>
   );
 };
