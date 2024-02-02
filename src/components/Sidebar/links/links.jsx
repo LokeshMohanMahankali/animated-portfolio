4c437c498f6f20cfb3770912073a176f1b5b0a62
Lokesh Mahanakali <149757667+LokeshMohanMahankali@users.noreply.github.com>
2024-02-02 07:54:36 +0530

    first-commit

--- src/components/Sidebar/links/links.jsx
+++ src/components/Sidebar/links/links.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const links = () => {
-  return <div>Links</div>;
+  return <div className="links">links</div>;
 };
 
 export default links;
--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -7,18 +7,35 @@ import Togglebutton from "./togglebutton/togglebutton";
 const sidebar = () => {
   const [open, setopen] = useState(false);
 
-  const varient = {
-    open:
-   closed:
+  const varients = {
+    open:{
+      clipPath:"circle(1200px at 50px 50px)",
+      Transition:{
+        type:"spring",
+        stiffness:20,
+        delay:80
+      },
+    },
+   closed:{
+    clipPath: "circle(30px at 50px 50px)",
+    Transition:{
+      delay:1,
+      type:"spring",
+      stiffness:400,
+      damping:40,
+    }
+   }
   }
 
-  return  <motion.div className="sidebar">
-      <motion.div className="bg">
+  return  (
+  <motion.div className="sidebar" animate= {open ? "open" : "closed"}>
+      <motion.div className="bg" variants={varients}>
         <Links />
       </motion.div>
       <Togglebutton setopen={setopen} />
     </motion.div>
-
+  
+  )
 };
 
 export default sidebar;
