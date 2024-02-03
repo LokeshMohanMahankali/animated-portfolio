d5bc1b10a4b31cbc47c662c701fa4d4f17738996
Lokesh Mahanakali <149757667+LokeshMohanMahankali@users.noreply.github.com>
2024-02-03 15:51:11 +0530

    first-commit

--- src/components/Sidebar/links/links.jsx
+++ src/components/Sidebar/links/links.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0, 
+  },
+};
 
 const links = () => {
   const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
 
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`}>{item}</a>
+        <motion.a key={item} href={`#${item}`} variants={itemVariants}>
+          {item}
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
